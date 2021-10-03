29c049dadfaf416dd36ccba33840058fd08f53f6
Gustavo Quino <31318966+gquinodeveloper@users.noreply.github.com>
2021-10-02 19:14:42 -0500

    Update 10_Getter_y_Setter.dart
    Dart 2.0

--- 10_Getter_y_Setter.dart
+++ 10_Getter_y_Setter.dart
@@ -1,28 +1,32 @@
 void main() {
  
   //GETTER Y SETTER
- /*
-Los getter y setter no son nada nuevo, estos forman parte del   concepto de "encapsulamiento" de la POO que nos permiten manipular atributos privados sin acceder directamente a ellos.
-    
-  */
+  //Los getter y setter no son nada nuevo, estos forman parte 
+  //del   concepto de "encapsulamiento" de la POO que nos permiten 
+  //manipular atributos privados sin acceder directamente a ellos.
+  
+  //Final: Su valor puede ser en tiempo de compilación
   final user = new User();
   user.id = 1;
-  user.name = "sdfsdfsdfs";
+  user.name = "Flutter";
   
   user.printValue();
 }
 
 class User {
-
-       int _id;
-       String _name;
+      
+      //late: indica que su valor va ser asignado posteriormente
+      late int _id;
+      String? _name;
 
       int get id => _id;
       set id(int value) {
         _id = value;
       }
-
-      String get name => _name;
+      
+      //En este caso la propieda indica que puede aceptar valores nulos
+      //entonces realizamos una validación con el ?? (significa entonces, si es null ?? ponle un vacío)
+      String get name => _name ?? "";
       set name(String value) {
         if(value.length > 7)
            _name = value;
